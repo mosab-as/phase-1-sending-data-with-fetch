9f6b2c228f92a33e992fa045dd6d408f0c661f35
admin <nour.majed1234@gmail.com>
2023-02-04 20:02:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+const submitData = (name, email) => {
+    return fetch(window.location.origin+"/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name,email,
+      }),
+    })
+      .then((response) => response.json())
+      .then((object) => {
+        document.body.innerHTML = object.id;
+      })
+      .catch((error) => {
+        document.body.innerHTML = error.message;
+      });
+  };
+  
+  submitData("Byron", "Byron@gmail.com");
